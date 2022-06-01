66d1a150e8d138b32c2df3e6df24dba988858b35
jorge <101826194+jorgeaguil@users.noreply.github.com>
2022-06-01 11:20:04 -0500

    Update 1.1linked-problems.js

--- 3p/1linkedlist/1/2do/1.1linked-problems.js
+++ 3p/1linkedlist/1/2do/1.1linked-problems.js
@@ -19,3 +19,29 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+function Nodo(datos) {
+    this.datos = datos
+    this.next = null
+}
+const traverse=(n)=>{//es6
+    let c=n//iterador
+    while(c){//exists
+        if(c.data)return n
+        c=c.next
+    }
+    return null
+}
+const contains = (v) => {
+    if(traverse(v)==v)return true
+    return false
+
+}
+let n1 = new Nodo(3)
+let n2 = new Nodo(-2)
+let n3 = new Nodo(1)
+n1.next=n2
+n2.next=n3
+let n=0
+
+console.log(contains(n2))
+console.log(n1)
