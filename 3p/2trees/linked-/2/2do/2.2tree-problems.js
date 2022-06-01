f887605fcfe604a6dc9d9501ec0c8cf3933d7726
jorge <101826194+jorgeaguil@users.noreply.github.com>
2022-06-01 11:21:56 -0500

    Update 2.2tree-problems.js

--- 3p/2trees/linked-/2/2do/2.2tree-problems.js
+++ 3p/2trees/linked-/2/2do/2.2tree-problems.js
@@ -25,3 +25,48 @@
  * posorder(root)
  * 
  */
+function inorder(n){
+    if (n!==null){
+        inorder(n.l)
+        console.log(n.d)
+        inorder(n.r)
+    }
+}
+function postorder(n){
+    if (n!==null){
+        postorder(n.l)
+        postorder(n.r)
+        console.log(n.d)
+    }
+}
+function preorder(n){
+    if (n!==null){
+        console.log(n.d)
+        preorder(n.l)
+        preorder(n.r)
+    }
+}
+
+function Node(o){
+    this.d=o
+    this.l=null
+    this.r=null
+}
+
+let p = new Node('+')
+let o1 = new Node('-')
+let o2 = new Node(3)
+let o3 = new Node(8)
+let o4 = new Node(3)
+
+p.l = o1
+o1.l = o3
+o1.r = o4
+p.r = o2
+
+console.log('Preorder:')
+console.log(preorder(p))
+console.log('Inorder')
+console.log(inorder(p))
+console.log('Postorder')
+console.log(postorder(p))
