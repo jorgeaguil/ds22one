6247032b3de4cc0e1bd050177cd59c88c582e672
jorge <101826194+jorgeaguil@users.noreply.github.com>
2022-06-01 15:55:30 -0500

    Update 1.2linked-problems.js

--- 3p/1linkedlist/2/2do/1.2linked-problems.js
+++ 3p/1linkedlist/2/2do/1.2linked-problems.js
@@ -20,3 +20,30 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+  class Randoms {
+      constructor() {
+        this.data = Array.from({ length: 1000 }, () => "name" + Math.floor(Math.random() * 1000));
+      }
+
+      findnodo(number) {
+        return this.data.find(item => item === "name" + number)
+      }
+
+      find7() {
+        const find7 = this.findnodo(7)
+        console.log("el numero encontrado", find7)
+        return !!find7
+      }
+      find3007() {
+        const find3007 = this.findnodo(3007)
+        return !!find3007
+      }
+      getRandoms() {
+        return this.data
+      }
+    }
+    const randomNumbers = new Randoms
+
+    console.log(randomNumbers.getRandoms())
+    console.log("node 7: ", randomNumbers.find7())
+    console.log("node 3007: ", randomNumbers.find3007())
